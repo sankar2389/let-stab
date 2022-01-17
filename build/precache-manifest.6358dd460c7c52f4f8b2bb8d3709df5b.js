cbbdd9eaef3a22c1079d0b024e052eb56e32705a
DESKTOP-NESQ3OS\msi <sankar2389@gmail.com>
2022-01-17 14:27:50 +0530

    build_commit

---
+++ build/precache-manifest.6358dd460c7c52f4f8b2bb8d3709df5b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a350b6b7e57ba8df1cfa40149e53a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a1aee0111fad6f0de8",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "c79dc4bf409e2bb76485",
+    "url": "/static/css/main.0a241e83.chunk.css"
+  },
+  {
+    "revision": "b5a1aee0111fad6f0de8",
+    "url": "/static/js/2.386f56cf.chunk.js"
+  },
+  {
+    "revision": "88acba1181dc48116778d847d6dad120",
+    "url": "/static/js/2.386f56cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79dc4bf409e2bb76485",
+    "url": "/static/js/main.f26e9093.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "1bffd0f56bbc84153293eaf8cc4bb68f",
+    "url": "/static/media/battery.1bffd0f5.jpg"
+  },
+  {
+    "revision": "090afd8afddd37ad69db330a733dd80f",
+    "url": "/static/media/broken.090afd8a.jpg"
+  },
+  {
+    "revision": "4ef04298552c6a773c5e81fee5a247f0",
+    "url": "/static/media/corn.4ef04298.jpg"
+  },
+  {
+    "revision": "1843f044fdc9c567ffc5427008a84029",
+    "url": "/static/media/finger.1843f044.jpg"
+  },
+  {
+    "revision": "a32cbe73319fbb52d7e1045d06efab90",
+    "url": "/static/media/fullhand.a32cbe73.jpg"
+  },
+  {
+    "revision": "43de6379238ee66cd8a603e4f94686cf",
+    "url": "/static/media/fullkey.43de6379.jpg"
+  },
+  {
+    "revision": "6b10daab68539e833ee9230140394d1d",
+    "url": "/static/media/fullkit.6b10daab.jpg"
+  },
+  {
+    "revision": "8a59a614c6ca50c3f5c98f33955e66a9",
+    "url": "/static/media/fullpouch.8a59a614.jpg"
+  },
+  {
+    "revision": "22817404786f897bde38fbc4096b065d",
+    "url": "/static/media/fulltype.22817404.jpg"
+  },
+  {
+    "revision": "b7d8d017fd1a55b6066457f6a104c334",
+    "url": "/static/media/hole.b7d8d017.jpg"
+  },
+  {
+    "revision": "036bf53003a4caebe21e6fe215533afe",
+    "url": "/static/media/key.036bf530.jpg"
+  },
+  {
+    "revision": "f0ad0b551fb3bd72d83aefd6eae8613b",
+    "url": "/static/media/lapens.f0ad0b55.jpg"
+  },
+  {
+    "revision": "eed43f2b3fbed657fa673b28eb4109d2",
+    "url": "/static/media/losepen.eed43f2b.jpg"
+  },
+  {
+    "revision": "986f193d27d37d047855ed95b6a44bf5",
+    "url": "/static/media/palm.986f193d.jpg"
+  },
+  {
+    "revision": "cece3959022331065ec24bfab96cd5ba",
+    "url": "/static/media/pendrive.cece3959.jpg"
+  },
+  {
+    "revision": "cec728271bc934c857669b7482ae0935",
+    "url": "/static/media/pouch.cec72827.jpg"
+  },
+  {
+    "revision": "0e69f2fdf89f9f8833342aa353a9d463",
+    "url": "/static/media/sidekit.0e69f2fd.jpg"
+  },
+  {
+    "revision": "0576cf2337d741aff971082e188fb20c",
+    "url": "/static/media/tab.0576cf23.jpg"
+  },
+  {
+    "revision": "c65785053e63bdc9c61e5c8e7c2abf56",
+    "url": "/static/media/thumb.c6578505.jpg"
+  },
+  {
+    "revision": "360abf4c4c8e3f6def14a4f1b521da0a",
+    "url": "/static/media/type.360abf4c.jpg"
+  }
+]);
